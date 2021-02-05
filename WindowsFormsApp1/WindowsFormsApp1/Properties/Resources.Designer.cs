fcf0362ee0a87bcbc4b69aa8f17bde82eaf0512e
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2021-02-04 21:54:16 -0500

    Update

--- WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DesktopAlarm.Properties {
+namespace BDODesktopAlarm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DesktopAlarm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DesktopAlarm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BDODesktopAlarm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
